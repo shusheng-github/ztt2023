d50096307a8ae5b1ce57de53d087258effd78796
jiawanjun <jiawanjun@58.com>
2023-08-21 23:10:56 +0800

    feat: 字母分組

--- code/49.字母异位词分组.js
+++ code/49.字母异位词分组.js
@@ -10,7 +10,19 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-
+  const map = new Map();
+  for (let str of strs) {
+      let array = Array.from(str);
+      array.sort();
+      let key = array.toString();
+      let list = map.get(key) ? map.get(key) : new Array();
+      list.push(str);
+      map.set(key, list);
+  }
+  return Array.from(map.values());
 };
+let strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+const result = groupAnagrams(strs)
+console.log('result', result)
 // @lc code=end
 
