6a7a0901eab5cc7617e993cefb43acf9643a618f
zhangtiantian08 <zhangtiantian08@58.com>
2023-08-25 18:19:34 +0800

    fix: 1

--- code/42.接雨水.js
+++ code/42.接雨水.js
@@ -4,6 +4,8 @@
  * [42] 接雨水
  */
 
+const { max } = require("lodash");
+
 // @lc code=start
 /**
  * @param {number[]} height
@@ -11,28 +13,48 @@
  */
 var trap = function (height) {
     const length = height.length;
+    let maxLeft = [];
+    let maxRight = [];
+    maxLeft[0] = height[0];
+    maxRight[length - 1] = height[length - 1];
+    let res = 0;
+    for (let i = 1; i < length; i++) {
+        maxLeft[i] = Math.max(maxLeft[i - 1], height[i]);
+    }
+    console.log("maxLeft :>> ", maxLeft);
+    for (let i = length - 2; i >= 0; i--) {
+        maxRight[i] = Math.max(height[i], maxRight[i + 1]);
+    }
+    for (let i = 0; i < length - 1; i++) {
+        const value = Math.min(maxLeft[i], maxRight[i]) - height[i];
+        res = res + value;
+    }
+    console.log("maxRight :>> ", maxRight);
+    console.log("res :>> ", res);
+};
+
+// 双指针
+var trap2 = function (height) {
+    const length = height.length;
     let left = 0;
-    let right = left + 1;
-    let totals = 0;
-    let add = 0;
-    while (left < length - 2 && right < length - 1) {
-        if (height[left] <= height[left]) {
+    let right = length - 1;
+    let lMax = 0;
+    let rMax = 0;
+    let res = 0;
+    while (left < right) {
+        lMax = Math.max(lMax, height[left]);
+        rMax = Math.max(rMax, height[right]);
+        if (lMax < rMax) {
+            res += lMax - height[left];
             left++;
-            right++;
         } else {
-            right++;
-            if (height[left] > height[left]) {
-                add += height[left] - height[right];
-            } else {
-                totals += add;
-                left = right;
-                right = left + 1;
-            }
+            res += rMax - height[right];
+            right--;
         }
     }
-    console.log('totals :>> ', totals);
+    return res
 };
 var height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]; // 6
 var height2 = [4, 2, 0, 3, 2, 5]; // 9
-trap(height);
+trap2(height2);
 // @lc code=end
