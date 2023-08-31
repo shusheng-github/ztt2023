ef7fbac5d6b4b7e61ec4109d9dd31ccce50ad43b
zhangtiantian08 <zhangtiantian08@58.com>
2023-08-31 19:43:30 +0800

    fix: 239

--- code/test.js
+++ code/test.js
@@ -1,20 +1,23 @@
 function addSerial(arr) {
     const length = arr.length;
     const map = new Map();
+    const map2 = new Map();
     const res = new Array(length);
-    for (var i=0; i < arr.length; i++) {
+    for (var i = 0; i < arr.length; i++) {
         const item = arr[i];
         if (!map.get(item)) {
             map.set(item, 1);
+            map2.set(item, i);
             res[i] = `${item}1`;
         } else {
             const count = map.get(item) + 1;
             map.set(item, count);
+            map2.delete(item)
             res[i] = `${item}${count}`;
         }
     }
-    console.log("map :>> ", map);
-    console.log('res :>> ', res);
+    console.log("map2 :>> ", map2);
+    console.log("res :>> ", res);
 }
 
 addSerial(["a", "b", "c", "a", "a", "b"]);
