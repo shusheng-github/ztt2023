4673e49f5840ade9617a92a04b400163d694a6a7
zhangtiantian08 <zhangtiantian08@58.com>
2023-10-24 15:42:57 +0800

    fix: 翻转数组

--- code/189.轮转数组.js
+++ code/189.轮转数组.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2023-10-08 14:50:53
- * @LastEditors: Shusheng
- * @LastEditTime: 2023-10-22 23:29:10
- * @FilePath: /common-business-yanzhen-buy/Users/a58/work/my-project/2023/code/189.轮转数组.js
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-24 15:39:42
+ * @FilePath: /hx-personal-center/Users/a58/work/my-project/2023/code/189.轮转数组.js
  */
 /*
  * @lc app=leetcode.cn id=189 lang=javascript
@@ -16,7 +16,7 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var rotate = function (nums, k) {
+var rotate3 = function (nums, k) {
     const n = nums.length;
     const newArr = new Array(n);
     for (let i = 0; i < n; i++) {
@@ -35,7 +35,29 @@ var rotate2 = function (nums, k) {
         nums[i] = newArr[i];
     }
 };
-
+// 翻转数组
+let reverse = function (nums, start, end) {
+    while (start < end) {
+        [nums[start], nums[end]] = [nums[end], nums[start]];
+        start++;
+        end--;
+    }
+};
+let rotate = function (nums, k) {
+    k %= nums.length;
+    // 先将整个数组翻转
+    reverse(nums, 0, nums.length - 1);
+    // k之前的翻转
+    reverse(nums, 0, k - 1);
+    // 之后的翻转
+    reverse(nums, k, nums.length - 1);
+    return nums;
+};
+// k = 3
+// i=0; i+k=3; newArr[3]=nums[0]
+// i=1; i+k=4; newArr[4]=nums[1]
+// i=3; i+k=6; newArr[0]=nums[3]
+// i=4; i+k=7; newArr[1]=nums[4]
 const arr = [1, 2, 3, 4, 5, 6, 7];
 [5, 6, 7, 1, 2, 3, 4];
 rotate(arr, 3);
---
+++ code/test.js
@@ -0,0 +1,12 @@
+/*
+ * @Date: 2023-10-24 14:26:44
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-24 14:28:14
+ * @FilePath: /2023/code/test.js
+ */
+const numberArray = [40, 1, 5, 200];
+// numberArray.sort((a, b) => {
+//     return a - b;
+// });
+const aa = numberArray.join();
+console.log("aa :>> ", aa);
