3efaf5a9fbdc131b5aee9c3673b8de1b06e98817
jiawanjun <jiawanjun@58.com>
2023-10-26 23:46:49 +0800

    fix: 1

---
+++ code/234.回文链表.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// 思路：快慢指针
+// 我们可以将链表的后半部分反转（修改链表结构），然后将前半部分和后半部分进行比较。比较完成后我们应该将链表恢复原样
+// 整个流程可以分为以下五个步骤：
+
+// 找到前半部分链表的尾节点。
+// 反转后半部分链表。
+// 判断是否回文。
+// 恢复链表。
+// 返回结果。
+const reverseList = (head) => {
+  let prev = null;
+  let curr = head;
+  while (curr !== null) {
+      let nextTemp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = nextTemp;
+  }
+  return prev;
+}
+
+const endOfFirstHalf = (head) => {
+  let fast = head;
+  let slow = head;
+  while (fast.next !== null && fast.next.next !== null) {
+      fast = fast.next.next;
+      slow = slow.next;
+  }
+  return slow;
+}
+
+var isPalindrome = function(head) {
+  if (head == null) return true;
+
+  // 找到前半部分链表的尾节点并反转后半部分链表
+  const firstHalfEnd = endOfFirstHalf(head);
+  const secondHalfStart = reverseList(firstHalfEnd.next);
+
+  // 判断是否回文
+  let p1 = head;
+  let p2 = secondHalfStart;
+  let result = true;
+  while (result && p2 != null) {
+      if (p1.val != p2.val) result = false;
+      p1 = p1.next;
+      p2 = p2.next;
+  }        
+
+  // 还原链表并返回结果
+  firstHalfEnd.next = reverseList(secondHalfStart);
+  return result;
+};
