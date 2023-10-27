2d1593868f194760d1370b2805c500be86f72c89
zhangtiantian08 <zhangtiantian08@58.com>
2023-10-27 11:36:09 +0800

    fix: 1

--- code/234.回文链表.js
+++ code/234.回文链表.js
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-10-27 11:03:56
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-27 11:35:23
+ * @FilePath: /2023/code/234.回文链表.js
+ */
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -9,7 +15,9 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-// 思路：快慢指针
+// 时间复杂度：O(n)，其中 n 指的是链表的大小。
+// 空间复杂度：O(1)，我们只会修改原本链表中节点的指向，而在堆栈上的堆栈帧不超过 O(1)O(1)。
+// 思路：快慢指针 + 反链表法
 // 我们可以将链表的后半部分反转（修改链表结构），然后将前半部分和后半部分进行比较。比较完成后我们应该将链表恢复原样
 // 整个流程可以分为以下五个步骤：
 
@@ -19,45 +27,45 @@
 // 恢复链表。
 // 返回结果。
 const reverseList = (head) => {
-  let prev = null;
-  let curr = head;
-  while (curr !== null) {
-      let nextTemp = curr.next;
-      curr.next = prev;
-      prev = curr;
-      curr = nextTemp;
-  }
-  return prev;
-}
+    let prev = null;
+    let curr = head;
+    while (curr !== null) {
+        let nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+};
 
 const endOfFirstHalf = (head) => {
-  let fast = head;
-  let slow = head;
-  while (fast.next !== null && fast.next.next !== null) {
-      fast = fast.next.next;
-      slow = slow.next;
-  }
-  return slow;
-}
+    let fast = head;
+    let slow = head;
+    while (fast.next !== null && fast.next.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
 
-var isPalindrome = function(head) {
-  if (head == null) return true;
+var isPalindrome = function (head) {
+    if (head == null) return true;
 
-  // 找到前半部分链表的尾节点并反转后半部分链表
-  const firstHalfEnd = endOfFirstHalf(head);
-  const secondHalfStart = reverseList(firstHalfEnd.next);
+    // 找到前半部分链表的尾节点并反转后半部分链表
+    const firstHalfEnd = endOfFirstHalf(head);
+    const secondHalfStart = reverseList(firstHalfEnd.next);
 
-  // 判断是否回文
-  let p1 = head;
-  let p2 = secondHalfStart;
-  let result = true;
-  while (result && p2 != null) {
-      if (p1.val != p2.val) result = false;
-      p1 = p1.next;
-      p2 = p2.next;
-  }        
+    // 判断是否回文
+    let p1 = head;
+    let p2 = secondHalfStart;
+    let result = true;
+    while (result && p2 != null) {
+        if (p1.val != p2.val) result = false;
+        p1 = p1.next;
+        p2 = p2.next;
+    }
 
-  // 还原链表并返回结果
-  firstHalfEnd.next = reverseList(secondHalfStart);
-  return result;
+    // 还原链表并返回结果
+    firstHalfEnd.next = reverseList(secondHalfStart);
+    return result;
 };
