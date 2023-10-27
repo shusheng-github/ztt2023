b16af63f7d01c4f7c2a39ac1e7126d5977eb9c99
zhangtiantian08 <zhangtiantian08@58.com>
2023-10-27 18:32:37 +0800

    fix: 1

--- js/apply+bind+call/apply.js
+++ js/apply+bind+call/apply.js
@@ -1,3 +1,13 @@
+/*
+ * @Date: 2023-08-08 16:56:02
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-27 17:05:53
+ * @FilePath: /2023/js/apply+bind+call/apply.js
+ */
+// ● 将函数设置为对象的属性
+// ● 执行函数
+// ● 删除函数
+// ● 返回函数执行结果
 function applyMy(context, arr) {
     context = context ? Object(context) : window;
     context.fn = this;
--- js/apply+bind+call/call.js
+++ js/apply+bind+call/call.js
@@ -1,8 +1,20 @@
-function applyMy (context) {
-  context = context ? Object(context) : window;
-  context.fn = this;
-  const args = [...arguments].slice(1);
-  const result = context.fn(...args);
-  delete context.fn;
-  return result;
+/*
+ * @Date: 2023-08-08 16:56:02
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-27 17:06:26
+ * @FilePath: /2023/js/apply+bind+call/call.js
+ */
+// 判断传入上下文对象是否存在，如果不存在，则设置为 window 。
+// 处理传入的参数，截取第一个参数后的所有参数。
+// 将函数作为上下文对象的一个属性。
+// 使用上下文对象来调用这个方法，并保存返回结果。
+// 删除刚才新增的属性。
+// 返回结果。
+function applyMy(context) {
+    context = context ? Object(context) : window;
+    context.fn = this;
+    const args = [...arguments].slice(1);
+    const result = context.fn(...args);
+    delete context.fn;
+    return result;
 }
