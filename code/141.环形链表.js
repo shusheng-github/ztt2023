bacd33f6726fee5afd5cab0db30d94ff7287547f
zhangtiantian08 <zhangtiantian08@58.com>
2023-10-31 14:15:38 +0800

    fix: 1

--- code/141.环形链表.js
+++ code/141.环形链表.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2023-10-27 14:14:53
  * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
- * @LastEditTime: 2023-10-27 14:53:51
- * @FilePath: /hx-personal-center/Users/a58/work/my-project/2023/code/141.环形链表.js
+ * @LastEditTime: 2023-10-31 14:14:53
+ * @FilePath: /2023/code/141.环形链表.js
  */
 /*
  * @lc app=leetcode.cn id=141 lang=javascript
@@ -26,15 +26,17 @@
 // 快慢指针
 // 如果有环,指针肯定会相遇
 var hasCycle = function (head) {
-    if (!head || !head.next) return false;
-    let slow = head;
+    if (head == null || head.next == null) {
+        return false;
+    }
     let fast = head.next;
-    while (slow !== fast) {
-        if (!fast || !fast.next) {
+    let last = head;
+    while (fast !== last) {
+        if (fast.next == null || fast.next.next == null) {
             return false;
         }
-        slow = slow.next;
         fast = fast.next.next;
+        last = last.next;
     }
     return true;
 };
--- code/142.环形链表 II.js
+++ code/142.环形链表 II.js
@@ -1,3 +1,9 @@
+/*
+ * @Date: 2023-10-31 11:15:26
+ * @LastEditors: zhangtiantian08 zhangtiantian08@58.com
+ * @LastEditTime: 2023-10-31 11:33:09
+ * @FilePath: /2023/code/142.环形链表 II.js
+ */
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -13,13 +19,36 @@
 // 时间复杂度：O(N)
 // 空间复杂度：O(N)
 var detectCycle = function (head) {
-  let set = new Set();
-  while (head !== null) {
-      if (set.has(head)) {
-          return head
-      }
-      set.add(head)
-      head = head.next
-  }
-  return null
+    let set = new Set();
+    while (head !== null) {
+        if (set.has(head)) {
+            return head;
+        }
+        set.add(head);
+        head = head.next;
+    }
+    return null;
+};
+
+// 时间复杂度：O(N)
+// 空间复杂度：O(1)
+var detectCycle = function (head) {
+    if (head == null) {
+        return null;
+    }
+    let fast = head;
+    let slow = head;
+    while (fast.next !== null && fast.next.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (fast === slow) {
+            let pre = head;
+            while (pre !== slow) {
+                slow = slow.next;
+                pre = pre.next;
+            }
+            return pre;
+        }
+    }
+    return null;
 };
